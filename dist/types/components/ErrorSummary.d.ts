c6f34a3b354ff02cf3752f0d98dd1e38b90ffae2
Chris Devereux <chrisdevereux@users.noreply.github.com>
2025-04-02 15:19:10 +0100

    update errorsummary component types

--- dist/types/components/ErrorSummary.d.ts
+++ dist/types/components/ErrorSummary.d.ts
@@ -6,13 +6,9 @@ export interface ErrorSummaryProps extends Omit<GovUKErrorSummaryProps, "errorLi
     errorList?: ErrorSummaryItem[];
 }
 interface ErrorSummaryItem {
-    /** Href attribute for the error link item. If provided item will be an anchor. **/
-    href?: string;
-    /** If `html` is set, this is not required. Text for the error link item. If `html` is provided, the `text` option will be ignored. **/
+    /** Text for the error link item. */
     content: Child;
-    /** If `text` is set, this is not required. HTML for the error link item. If `html` is provided, the `text` option will be ignored. **/
-    html: string;
-    /** HTML attributes (for example data attributes) to add to the error link anchor. **/
+    /** HTML attributes (for example data attributes) to add to the error link anchor. */
     attributes?: Record<string, unknown>;
 }
 export declare function ErrorSummary({ title, description, ...props }: ErrorSummaryProps): Promise<import("hono/utils/html").HtmlEscapedString>;
--- src/components/ErrorSummary.tsx
+++ src/components/ErrorSummary.tsx
@@ -20,16 +20,10 @@ export interface ErrorSummaryProps
 }
 
 interface ErrorSummaryItem {
-  /** Href attribute for the error link item. If provided item will be an anchor. **/
-  href?: string;
-
-  /** If `html` is set, this is not required. Text for the error link item. If `html` is provided, the `text` option will be ignored. **/
+  /** Text for the error link item. */
   content: Child;
 
-  /** If `text` is set, this is not required. HTML for the error link item. If `html` is provided, the `text` option will be ignored. **/
-  html: string;
-
-  /** HTML attributes (for example data attributes) to add to the error link anchor. **/
+  /** HTML attributes (for example data attributes) to add to the error link anchor. */
   attributes?: Record<string, unknown>;
 }
 
